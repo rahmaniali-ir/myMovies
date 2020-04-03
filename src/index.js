7f7caef98ab0176831fef56cfc38ef529078a088
Ali Rahmani <alirahmani.prog@gmail.com>
2020-04-04 00:20:29 +0430

    Rewriting explorer, clean up, loading wait added

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 const { exec } = require("child_process");
 const Bank = require('../lib/bank');
 const { dialog } = electron.remote;
-const defaults = require('../lib/defaults');
-const checkTargetContents = require('../lib/explorer');
+let settings = require('../lib/settings');
+const checkTargetFolders = require('../lib/explorer');
 
 // database connection
 const myBank = new Bank(path.join(__dirname, '../database.db'));
@@ -18,8 +18,8 @@ let movies = myBank.table('movies');
 
 // general variables
 let newMoviesCount = 0;
-let allowedExtensions = defaults.allowedExtensions;
-const apiKey = defaults.apiKey;
+let allowedExtensions = settings.allowedExtensions;
+const apiKey = settings.apiKey;
 
 function checkForConnection() {
     console.log(navigator.onLine ? 'Online' : 'Offline');
@@ -28,17 +28,43 @@ checkForConnection();
 window.addEventListener('online', checkForConnection);
 window.addEventListener('offline', checkForConnection);
 
+// opens dialog to select a folder as a target folder
+function showTargetDialog() {
+    dialog.showOpenDialog({
+        properties: ['openDirectory']
+    }).then(res => {
+        const selectedFolder = res.filePaths[0];
+        if(selectedFolder) {
+            // console.log(targetFolders);
+            targetFolders.insert({ path: selectedFolder });
+        }
+    });
+}
+
 // show "select target folders" dialog if there is no target folder
 if(targetFolders.length < 1) {
-    // showTargetDialog();
+    showTargetDialog();
 }
 // showTargetDialog();
 
-// check all files for existence and add them to database if not
-targetFolders.data.forEach(target => {
-    if(fs.existsSync(target.path)) {
-        checkTargetContents(target.path);
+function hideLoading() {
+    document.querySelector('.loading').classList.add('done');
+}
+
+let time = 0;
+let loadingDone = false;
+let timer = setInterval(() => {
+    time++;
+
+    if(loadingDone && time > 3) {
+        hideLoading();
+        clearInterval(timer);
     }
+}, 1000);
+
+checkTargetFolders(newMovies => {
+    console.log('All done!', newMovies + ' new movies discovered!');
+    loadingDone = true;
 });
 
 // handle window state buttons
@@ -47,25 +73,5 @@ const windowStates = {
     '#minimize': () => currentWindow.minimize(),
     '#maximize': () => currentWindow.isMaximized() ? currentWindow.restore() : currentWindow.maximize()
 };
-
 for(state in windowStates)
     document.querySelector(state).addEventListener('click', windowStates[state]);
-
-// movies.empty();
-
-setTimeout(() => {
-    document.querySelector('.loading').classList.add('done');
-}, 5000);
-
-movies.data.forEach(movie => {
-    // let movieCard = new MovieCard();
-    // movieCard.title = movie.name;
-    // movieCard.image = movie.Poster;
-    // document.querySelector('.movies-wrapper').append(movieCard);
-});
-
-// const n = new Notif();
-// n.title = 'Hello';
-// n.text = 'Description';
-// document.querySelector('main').append(n);
-// window.customElements.define('notif-box', Notif);
